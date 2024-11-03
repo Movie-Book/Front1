9a8e8d5ff343b2d427fffa4e592f5743c91a9e4b
yeriel1204 <yeriel1204@naver.com>
2024-11-04 04:47:54 +0900

    modified:   src/components/BottomNavigationBar.jsx 	modified:   src/components/Logo.jsx 	modified:   src/components/LogoWithMypage.jsx 	new file:   src/components/Movie/EditGenre.js 	new file:   src/components/Movie/EditTaste.js 	new file:   src/components/Movie/MovieRateEdit.js 	modified:   src/components/MovieRateDialog.jsx 	modified:   src/components/Profile/EditUser.js 	modified:   src/components/Profile/FriendList.js 	modified:   src/components/Stars.jsx 	modified:   src/css/ProfileLayout.css 	modified:   src/index.js 	renamed:    src/pages/LoginLayout.jsx -> src/pages/LoginLayout.js 	deleted:    src/pages/MyMovie.js 	new file:   src/pages/MyMovieLayout.js 	renamed:    src/pages/ProfileLayout.jsx -> src/pages/ProfileLayout.js

--- src/components/Movie/MovieRateEdit.js
+++ src/components/Movie/MovieRateEdit.js
@@ -1,38 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Stars from "./Stars";
 
-function MovieRateDialog({ openModal, movieTitle, moviePoster, rate, onRateSubmit }) {
+function MovieRateDialog({openModal, movieTitle, moviePoster, rate}){
     const modalRef = useRef();
-    const newRate = useRef(rate); 
 
     const modalClose = () => {
         modalRef.current.close();
-        if (onRateSubmit) {
-            onRateSubmit(newRate.current); 
-        }
-    };
-
-    if (openModal) {
-        modalRef.current.showModal();
     }
+    if(openModal)
+        modalRef.current.showModal();
 
-    const handleRateChange = (updatedRate) => {
-        newRate.current = updatedRate;
-    };
-
-    return (
+    return(
         <div>
             <dialog ref={modalRef} className="movieRate">
                 <h2>별점 수정하기</h2>
                 <img src={moviePoster} alt={movieTitle} />
                 <div className="movieInfo">
                     <h5 className="movieTitle">{movieTitle}</h5>
-                    <Stars rate={rate} onRateChange={handleRateChange} />
+                    <Stars rate={rate}/>
                 </div>
                 <button onClick={modalClose} className="dialogButton">확인</button>
             </dialog>
         </div>
-    );
+    )
 }
 
 export default MovieRateDialog;
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import { FaRegStar, FaStar } from 'react-icons/fa';
 
-function Stars({rate}){
+function Stars({ rate, onRateChange }) {
     const [rateScore, setRateScore] = useState(rate);
 
-    return(
+    const handleStarClick = (newRate) => {
+        setRateScore(newRate);
+        if (onRateChange) {
+            onRateChange(newRate);  
+        }
+    };
+
+    return (
         <div>
-            {[...Array(rateScore)].map((a, i) => (
-                <FaStar className="star-lg" key={i} onClick={() => setRateScore(i + 1)} />
+            {[...Array(rateScore)].map((_, i) => (
+                <FaStar className="star-lg" key={i} onClick={() => handleStarClick(i + 1)} />
             ))}
-            {[...Array(5 - rateScore)].map((a, i) => (
-                <FaRegStar className="star-lg" key={i} onClick={() => setRateScore(rateScore + i + 1)} />
+            {[...Array(5 - rateScore)].map((_, i) => (
+                <FaRegStar className="star-lg" key={i} onClick={() => handleStarClick(rateScore + i + 1)} />
             ))}
-    </div>
+        </div>
     );
 }
 
