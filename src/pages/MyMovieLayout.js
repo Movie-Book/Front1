f5396f2c80c2a987d42bb6f74a2d9d9ae2b12a6d
김민정 <48087461+yeriel1204@users.noreply.github.com>
2024-11-19 02:54:29 +0900

    Add files via upload

--- src/pages/MyMovieLayout.js
+++ src/pages/MyMovieLayout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import EditGenre from '../components/Movie/EditGenre';
 import EditTaste from '../components/Movie/EditTaste';
+import EditFavoriteGenre from '../components/Movie/EditFavoriteGenre';
+import EditHateGenre from '../components/Movie/EditHateGenre';
 import MyMovie from '../components/Movie/MyMovie';
 import '../css/index.css';
 import '../css/MyMovieLayout.css';
@@ -14,6 +16,8 @@ const MyMovieLayout = () => {
         <Route path="/" element={<MyMovie />} />
         <Route path="/taste" element={<EditTaste />} />
         <Route path="/genre" element={<EditGenre />} />
+        <Route path="/editfavoritegenre" element={<EditFavoriteGenre />} />
+        <Route path="/edithategenre" element={<EditHateGenre />} />
       </Routes>
     </div>
   );
