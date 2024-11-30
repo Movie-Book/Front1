716712e956d99d5161e2f69b3a04f6a5ce2d505a
sujin209 <20201036@sungshin.ac.kr>
2024-11-30 11:01:25 +0900

    중복된거수정

--- src/pages/MyMovieLayout.js
+++ src/pages/MyMovieLayout.js
@@ -7,8 +7,6 @@ import MyMovie from '../components/Movie/MyMovie';
 import '../css/index.css';
 import '../css/MyMovieLayout.css';
 import MyGenre from '../components/Movie/MyGenre';
-import EditFavoriteGenre from '../components/Movie/EditFavoriteGenre';
-import EditHateGenre from '../components/Movie/EditHateGenre';
 
 
 const MyMovieLayout = () => {
