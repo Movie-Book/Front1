765b2e786cc1f3173b8ee6ee5e4079b1cdddf4e4
yereil1204 <yeriel1204@naver.com>
2024-12-09 13:58:08 +0900

    modified:   src/components/BookTasteButton.jsx 	modified:   src/components/MovieTasteButton.jsx

--- src/components/BookTasteButton.jsx
+++ src/components/BookTasteButton.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Stars from "./Stars";
 
 function BookTasteButton(props) {
+    const truncateTitle = (title, maxLength) => {
+        if (title.length > maxLength+1) {
+            return title.slice(0, maxLength) + "⋯"; 
+        }
+        return title; 
+    };
 
-    return(
+    return (
         <div className="bookButton" onClick={props.onClick}>
             <img src={props.bookImage} alt={props.bookTitle} />
-            <h5 className="bookButtonTitle">{props.bookTitle}</h5>
-            <div className="stars"><Stars  rate={props.rate} rating = {false}/></div>
+            <h5 className="bookButtonTitle">
+                {truncateTitle(props.bookTitle, 20)} 
+            </h5>
+            <div className="stars">
+                <Stars rate={props.rate} rating={false} />
+            </div>
         </div>
     );
 }
--- src/components/MovieTasteButton.jsx
+++ src/components/MovieTasteButton.jsx
@@ -4,11 +4,17 @@ import Stars from "./Stars";
 function MovieTasteButton(props) {
 
     const selected = props.selected ? 'selected' : '';
+    const truncateTitle = (title, maxLength) => {
+        if (title.length > maxLength+1) {
+            return title.slice(0, maxLength) + "⋯"; 
+        }
+        return title; 
+    };
 
     return(
         <div className="movie" onClick={props.onClick}>
             <img className = {selected} src={props.moviePoster} alt={props.movieTitle} />
-            <h5 className="movieTitle">{props.movieTitle}</h5>
+            <h5 className="movieTitle">{truncateTitle(props.movieTitle, 20)}</h5>
             <div className="stars"><Stars  rate={props.rate} rating = {false}/></div>
         </div>
     );
